b8c74f0656752da59d8dcd32829f59d7d4cea824
Anatoly Sedov <a.sedov@uniyar.ac.ru>
2017-05-15 21:51:20 +0300

    еще переделки

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ from model import init_model
 from numpy import genfromtxt
 import sys
 
+marking = genfromtxt("training/marking.txt", dtype='int',delimiter=',')
+
+data = np.zeros((marking.shape[0],cnf.square_size, cnf.square_size, 3), dtype=np.float)
+
+tlp = TileDataProvider(cnf.map_source, cnf.square_size)
+
+for x in range(0,marking.shape[0]):
+    im = tlp.load_tile_data(marking[x][0],marking[x][1])
+    data[x] = im
+
+tlp.destruct()
+
+x_train = data[0:cnf.training_count:1]   #обучающее множество
+x_test  = data[cnf.training_count:100:1] #проверочное множество
+
+y_train = keras.utils.to_categorical(marking[0:cnf.training_count,2], num_classes=5)
+y_test = keras.utils.to_categorical(marking[cnf.training_count:100,2], num_classes=5)
+
 if(len(sys.argv)>1 and sys.argv[1]=="train"):
-    marking = genfromtxt("training/marking.txt", dtype='int',delimiter=',')
-    
-    data = np.zeros((marking.shape[0],cnf.square_size, cnf.square_size, 3), dtype=np.float)
-    
-    tlp = TileDataProvider(cnf.map_source, cnf.square_size)
-    
-    for x in range(0,marking.shape[0]):
-        im = tlp.load_tile_data(marking[x][0],marking[x][1])
-        data[x] = im
-    
-    tlp.destruct()
-    
-    x_train = data[0:cnf.training_count:1]   #обучающее множество
-    x_test  = data[cnf.training_count:100:1] #проверочное множество
-    
-    y_train = keras.utils.to_categorical(marking[0:cnf.training_count,2], num_classes=5)
-    y_test = keras.utils.to_categorical(marking[cnf.training_count:100,2], num_classes=5)
-    
     model = init_model()
 
     sgd = SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
@@ -36,6 +36,7 @@ if(len(sys.argv)>1 and sys.argv[1]=="train"):
     model.fit(x_train, y_train, batch_size=20, epochs=400)
     model.save_weights('./model_weights.h5')
 elif(len(sys.argv)>1 and sys.argv[1]=="load"):
+    model = init_model()
     model.load_weights('./model_weights.h5')
 else:
     print("usage: main.py train|load")
