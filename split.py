3d43646647fc9a1014aad2a640faac0e4101eaa1
Stanislav Tsaplev <stanislav.tsaplev@gmail.com>
2017-05-15 11:05:36 +0300

    мелкие правки
    В файлах config.py и split.py установлена кодировка utf-8.
    Некоторые имена переменных изменены на более читаемые.
    Деление в функции split_to_tiles заменено на целочисленное (python3
    only).

--- split.py
+++ split.py
@@ -1,19 +1,21 @@
+# -*- coding: utf-8 -*-
+
 import config as cnf
 from PIL import Image
 
 # Скриптик для того чтобы разбить картинку на квадратики
+def split_to_tiles(path_to_save, input_file_name, square_size):
+    image = Image.open(input_file_name)
+    image_width, image_height = image.size
 
-def split_to_tiles(save_path, input, size):
-    im = Image.open(input)
-    imgwidth, imgheight = im.size
-
-    for i in range(0,imgheight,size):
-        for j in range(0,imgwidth,size):
-            box = (j, i, j+size, i+size)
-            a = im.crop(box)
+    for i in range(0, image_height, square_size):
+        for j in range(0, image_width, square_size):
+            box = (j, i, j + square_size, i + square_size)
+            tile = image.crop(box)
             
-            tile_address = save_path+"/tile-%d-%d.png"%(i/size,j/size)
-            a.save(tile_address)
+            filename = path_to_save + "/tile-%d-%d.png" % (i // square_size, j // square_size)
+            tile.save(filename)
+            print(filename + ' created')
             
 
 split_to_tiles(cnf.tiles, cnf.map_source, cnf.square_size)
